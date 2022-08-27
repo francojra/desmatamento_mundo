7604f651236a5ea0284b16c5914edf5d3c4216ff
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-08-26 23:36:43 -0300

    Add files via upload

---
+++ desmatamento_mundo.R
@@ -0,0 +1,18 @@
+
+# Desmatamento no mundo --------------------------------------------------------------------------------------------------------------------
+# Autora do script: Jeanne Franco ----------------------------------------------------------------------------------------------------------
+# Data: 25/08/22 ---------------------------------------------------------------------------------------------------------------------------
+# Referência: https://ourworldindata.org/working-hours -------------------------------------------------------------------------------------
+
+# Sobre os dados ---------------------------------------------------------------------------------------------------------------------------
+
+### A perda florestal líquida não é o mesmo que a desflorestação - ela mede a 
+### desflorestação mais quaisquer ganhos na floresta durante um determinado 
+### período.
+
+### Durante a década desde 2010, a perda líquida nas florestas a nível 
+### mundial foi de 4,7 milhões de hectares por ano. No entanto, as taxas 
+### de desflorestação foram muito mais elevadas.
+
+### A FAO da ONU estima que 10 milhões de hectares de floresta foram 
+### abatidos todos os anos.
